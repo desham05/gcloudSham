4881806a38066e4273c9d47e3fc73272c1bfe739
desham90 <s.dere.05@gmail.com>
2018-06-21 13:58:26 +0530

    updated

--- main.py
+++ main.py
@@ -6,30 +6,43 @@ import json
 import hashlib
 from copy import deepcopy
 import numpy as np
-import pandas as pd
+import pymysql
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 plt.rcParams['figure.figsize'] = (16, 9)
 plt.style.use('ggplot')
-data = pd.read_csv('titanic3.csv')
-data.head()
 
 app = Flask(__name__)
 
+dbServerName    = "35.237.45.39"
+dbUser          = "root"
+dbPassword      = "root"
+dbName          = "sqldb"
+charSet         = "utf8mb4"
+cusrorType      = pymysql.cursors.DictCursor
+
+ 
+
+connectionObject = pymysql.connect(host=dbServerName, user=dbUser, password=dbPassword,db=dbName, charset=charSet,cursorclass=cusrorType)
    
 def randrange():
-    lat = data['age'].values
-    long = data['fare'].values
-    X = np.array(list(zip(lat, long)))
-    # plt.scatter(lat, long, c='red', s=7)
-    # plt.show()
-    kmeans = KMeans(n_clusters=8)
+    cursor = connectionObject.cursor()
+    success='SELECT age,fare from titanic'
+    cursor.execute(success)
+    
+    result_set = cursor.fetchall()
+    age =[]
+    fare =[]	
+    for row in result_set:
+       age.append(row['age'])
+       fare.append(row['fare'])	   
+    X = np.array(list(zip(age, fare)))
+    kmeans = KMeans(n_clusters = int(8))
     kmeans.fit(X)
     centroid = kmeans.cluster_centers_
     labels = kmeans.labels_
 
     all = [[]] * 8
-    print(all)
     for i in range(len(X)):
 
         # print(index)
@@ -41,11 +54,62 @@ def randrange():
 
         plt.scatter(centroid[:, 0], centroid[:, 1], marker="x", s=150, linewidths=5, zorder=10)
         plt.show()
+        return render_template('success.html')		
 	
+def piechart():
+    cursor = connectionObject.cursor()
+    success='SELECT count(*) from titanic where sex="female" and survived ="1" group by pclass'
+    cursor.execute(success)
+    
+    result_set = cursor.fetchall()
+    age =[]
+    for row in result_set:
+       age.append(row['count(*)'])
 
+    labels = ('PC1', 'PC2', 'PC3')
+    colors = ['gold', 'yellowgreen', 'lightcoral']
+    sizes = [age[0],age[1],age[2]]
+    explode = (0.1, 0, 0)
+
+    plt.pie(sizes, explode=explode, labels=labels, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.axis('equal')
+    plt.show()
+
+    return render_template('success.html')
+	
+def barchart():
+    cursor = connectionObject.cursor()
+    success='SELECT count(*) from titanic where sex="male" and survived ="1" group by pclass'
+    cursor.execute(success)
+    
+    result_set = cursor.fetchall()
+    age =[]
+    for row in result_set:
+       age.append(row['count(*)'])
+   
+    objects = ('PC1', 'PC2', 'PC3')
+    y_pos = np.arange(len(objects))
+    performance = [age[0],age[1],age[2]]
+ 
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Count')
+    plt.title('numbers of male survivors')
+    plt.show()
+	
+    return render_template('success.html')
+		
 @app.route('/multiplerun', methods=['GET'])
 def randquery():
-    return randrange() 	
+    return randrange()
+
+@app.route('/pie', methods=['GET'])
+def piec():
+    return piechart()
+
+@app.route('/bar', methods=['GET'])
+def barc():
+    return barchart() 	
 
 @app.route('/')
 def hello_world():
